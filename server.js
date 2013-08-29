99ddf5a2ba562d56ea7b49998533a3670e213b9f
makem <makem@ukr.net>
2013-08-29 19:02:50 +0300

    Home page updated

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+var home = require('./routes/home');
 var http = require('http');
 var path = require('path');
 
@@ -22,12 +23,13 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', home.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
